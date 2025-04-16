156f6ec42a933151eeb24038964a84ccad9816a0
Shrihari Kulkarni <shriharik123@gmail.com>
2025-04-16 08:27:50 +0530

    merge sort

--- src/com/scaler/multithreading/MergeSort.java
+++ src/com/scaler/multithreading/MergeSort.java
@@ -1,64 +1,51 @@
 package com.scaler.multithreading;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Future;
 
-public class MergeSort implements Callable<Void> {
+public class MergeSort implements Callable<List<Integer>> {
 
-    private int[] arr;
-    private int start;
-    private int end;
+    private List<Integer> list;
     private ExecutorService executorService;
 
-    public MergeSort(int[] arr, int start, int end, ExecutorService executorService) {
-        this.arr = arr;
-        this.start = start;
-        this.end = end;
+    public MergeSort(List<Integer> list, ExecutorService executorService) {
+        this.list = list;
         this.executorService = executorService;
     }
 
 
     @Override
-    public Void call() throws Exception {
-        if (start >= end) {
-            return null;
+    public List<Integer> call() throws Exception {
+        if (list.size() <= 1) {
+            return new ArrayList<>(list);
         }
-        int mid = start + (end - start) / 2;
-        Callable<Void> leftSorted = new MergeSort(arr, start, mid, executorService);
-        Callable<Void> rightSorted = new MergeSort(arr, mid + 1, end, executorService);
-        Future<Void> future1 = executorService.submit(leftSorted);
-        Future<Void> future2 = executorService.submit(rightSorted);
-        future2.get();
-        future1.get();
-        int i = start, j = mid + 1, k = 0;
-        int[] temp = new int[end - start + 1];
-        // lets say start = 5, end = 8
-        // mid = 6
-        // 5 to 6 is sorted
-        // 7 to 8 is sorted
-        // now create a new array or size 8 - 5 + 1. and then merge them
-        while (i <= mid && j <= end) {
-            if (arr[i] < arr[j]) {
-                temp[k++] = arr[i++];
+        int mid = list.size() / 2;
+        Callable<List<Integer>> callable1 = new MergeSort(list.subList(0, mid), executorService);
+        Callable<List<Integer>> callable2 = new MergeSort(list.subList(mid, list.size()), executorService);
+        Future<List<Integer>> leftFuture = executorService.submit(callable1);
+        Future<List<Integer>> rightFuture = executorService.submit(callable2);
+        List<Integer> leftSorted = leftFuture.get();
+        List<Integer> rightSorted = rightFuture.get();
+        int i = 0, j = 0, k = 0;
+        List<Integer> answer = new ArrayList<>();
+        while (i < leftSorted.size() && j < rightSorted.size()) {
+            if (leftSorted.get(i) < rightSorted.get(j)) {
+                answer.add(leftSorted.get(i++));
             } else {
-                temp[k++] = arr[j++];
+                answer.add(rightSorted.get(j++));
             }
         }
 
-        while (i <= mid) {
-            temp[k++] = arr[i++];
+        while (i < leftSorted.size()) {
+            answer.add(leftSorted.get(i++));
         }
 
-        while (j <= end) {
-            temp[k++] = arr[j++];
+        while (j < rightSorted.size()) {
+            answer.add(rightSorted.get(j++));
         }
-
-        int t = start;
-
-        for (k = 0; k < temp.length; k++) {
-            arr[t++] = temp[k];
-        }
-        return null;
+        return answer;
     }
 }
--- src/com/scaler/multithreading/MergeSortMain.java
+++ src/com/scaler/multithreading/MergeSortMain.java
@@ -1,17 +1,19 @@
 package com.scaler.multithreading;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.*;
 
 public class MergeSortMain {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        int[] arr = new int[]{7, 8, 2, 1, 4, 5, 3, 6};
+        List<Integer> list = List.of(7,5,1,4,9,6,0,8);
         ExecutorService executorService = Executors.newFixedThreadPool(4);
-        Callable<Void> callable = new MergeSort(arr, 0, arr.length - 1, executorService);
-        Future<Void> future = executorService.submit(callable);
-        future.get();
-        System.out.println(Arrays.toString(arr));
-        executorService.shutdown();
+        Callable<List<Integer>> callable = new MergeSort(list, executorService);
+        Future<List<Integer>> future = executorService.submit(callable);
+        List<Integer> answer = future.get();
+        System.out.println(answer);
+        executorService.shutdownNow();
     }
 }
